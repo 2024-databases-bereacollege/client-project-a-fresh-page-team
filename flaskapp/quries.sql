8014aa3dfa938466a514304e365c2acd1c560ecf
Anupriya Dixit <112175467+dixita9@users.noreply.github.com>
2024-04-04 19:48:09 +0000

    my changes

---
+++ flaskapp/quries.sql
@@ -0,0 +1,11 @@
+--Documents:
+--1. Submit documents - (HIGH) - CREATE
+INSERT INTO "documentation" ("DOC_ID", "DO_ID", "FB_ID", "type_of_documentation", "date_obtained", "date_valid")
+VALUES
+    (32567, 55378, NULL, ‘Registration’, '2025-09-09', '2023-04-21')
+
+
+
+
+
+
