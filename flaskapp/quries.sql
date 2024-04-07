8b32de7fc56aa5362d3f491aa756ac24a8f42ebd
Anupriya Dixit <112175467+dixita9@users.noreply.github.com>
2024-04-07 12:55:18 +0000

    sql and peewee files created

--- flaskapp/quries.sql
+++ flaskapp/quries.sql
@@ -4,6 +4,16 @@ INSERT INTO "documentation" ("DOC_ID", "DO_ID", "FB_ID", "type_of_documentation"
 VALUES
     (32567, 55378, NULL, ‘Registration’, '2025-09-09', '2023-04-21')
 
+--2.View the details of a document - (HIGH) - READ 
+
+SELECT “DOC_ID”, “DO_ID”, “FB_ID”, type_of_documentation, date_obtained, date_valid 
+FROM documentation;
+
+
+
+
+
+
 
 
 
