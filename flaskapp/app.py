b8cd9e68b7f2e6822e8f46be67cca48843112ad4
vungc <112138502+vungc@users.noreply.github.com>
2024-04-10 21:09:37 +0000

    add some flask items

---
+++ flaskapp/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+from models import donor
+
+app = Flask(__name__)
+
+@app.route('/')
+def donorList():
+    donors = donor.select()
+    return render_template("donor.html", 
+                            page_title="Donor List",
+                            page_description="Choose from one of the available Donor", 
+                            donors=donors)
+@app.route('/see details', methods=['POST'])
+def register():
+    """
+    """
+    gg = request.form.get('DO_ID')
+
+    return 'Hi there'
